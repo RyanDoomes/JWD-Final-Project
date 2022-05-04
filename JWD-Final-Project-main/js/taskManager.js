6c7e7ad317ae9dff692c1f3d37079bb904306e7e
Charlene Cornelius <chichiwepe@aol.com>
2022-05-04 14:17:41 -0400

    task4Work

--- JWD-Final-Project-main/js/taskManager.js
+++ JWD-Final-Project-main/js/taskManager.js
@@ -1,5 +1,14 @@
 class TaskManager {
-    constructor() {
+    constructor(currentId) {
         this.tasks=[];
+        this.currentId = 0;
+        
+
+    }
+   
+    addTask(name,description,assignedTo,dueDate,status){
+        return this.currentId++;
     }
 }
+
+
