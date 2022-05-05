7ea8f052ca7f261a560d74b97691919964db9781
RyanDoomes <ryan.doomes@gmail.com>
2022-05-05 16:49:35 -0700

    session from the re do

--- JWD-Final-Project-main/js/index.js
+++ JWD-Final-Project-main/js/index.js
@@ -1,2 +1,4 @@
-const taskHtml = createTaskHtml();
-console.log(taskHtml);
+let task1 = new TaskManager;
+task1.addTask("take out the garbage", "Ryan", 05-05-2022, "take out at 6pm");
+
+console.log(task1);
--- JWD-Final-Project-main/js/taskManager.js
+++ JWD-Final-Project-main/js/taskManager.js
@@ -1,82 +1,12 @@
- const button = document.getElementById('createButton');
- const form = document.querySelector('form');
-
- button.addEventListener('click', createTaskHtml);
-
-
- const createTaskHtml = (taskName, assignedTo, dueDate, taskNote) => {
-     //document.getElementsByClass('cardTitle').innerHTML('task') = `This is the ${createTaskHtml}`;
-     const html = `<div class="container">
-  <div class="form-group">
-  <!--Task Name-->
-    <form action="index.html" method="GET" class="task-form">
-      <label for="taskName">
-      ${taskName}
-    
-      </label> 
-    
-    <input type="text" id="taskName" required>
-    <br>
-    
-  
-
-  <!--Assigned To-->
-
-  
-    <label for="assignedTo">
-      ${assignedTo}
-    </label>
-    <input type="text" id="assignedTo" required>
-    <br>
-  
-  
-
-  <!--Due Date-->
-
-  
-
-    <label for="dueDate">
-      ${dueDate}
-    </label>
-    <input type="date" id="dueDate" required>
-    <br>
- 
-
-  <!--Task Notes-->
-  
-  
-
-    <label for="taskNotes">
-    ${description}
-    </label>
-    <textarea id="taskNotes" required></textarea>
-    <br>
-    
-    <input type="submit" value="Create Task" id="createButton">
-  
-  </form>
-
-  return html} 
-
-
 class TaskManager {
-    constructor(currentId = 0) {
-        this.tasks = [];
-        this.currentId = currentId;
-    }
-
-    addTask(name, description, assignedTo, dueDate){
-      const task = {
-        id: this.currentId++,
-        name: name,
-        description: description,
-        assignedTo: assignedTo,
-        dueDate: dueDate,
-        status: 'TO DO'
-      };
-
-    
-
-    this.tasks.push(task);
-    }`
+  constructor(currentId = 0) {
+    this.tasks = [];
+    this.currentId = currentId;
   }
+  addTask(taskName, assignedTo, dueDate, taskNotes){
+    this.currentId++;
+    tasks.push(id, taskName, assignedTo, dueDate, taskNotes);
+  }
+
+
+}
