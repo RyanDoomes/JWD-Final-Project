85699147183b075654213ab7cfd1c93f26a3adb4
Charlene Cornelius <chichiwepe@aol.com>
2022-05-06 19:32:32 -0400

    adding new HTML

--- js/taskManager.js
+++ js/taskManager.js
@@ -1,3 +1,17 @@
+const createTaskHtml = (taskName, assignedTo, dueDate, taskNotes)=>{
+const newForm = `<li class="list-group-item">
+<div class="d-flex w-100 mt-2 justify-content-between align-items-center">
+    <h5>${taskName}</h5>
+    <span class="badge badge-danger">TODO</span>
+</div>
+<div class="d-flex w-100 mb-3 justify-content-between">
+    <small>Assigned To:${assignedTo}</small>
+    <small>Due:${dueDate}</small>
+</div>
+<p>${taskNotes}</p>
+</li>
+`;}
+
 class TaskManager {
   constructor(currentId = 0) {
     this.tasks = [];
