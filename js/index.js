7b4c7ac4380ce099e0d5752024895d5acc261db2
Charlene Cornelius <chichiwepe@aol.com>
2022-05-06 16:52:30 -0400

    re try

--- js/index.js
+++ js/index.js
@@ -2,3 +2,9 @@ let task1 = new TaskManager;
 
 const newTaskForm = document.querySelector('.task-form');
 
+newTaskForm.addEventListener('submit', (event) => {
+
+    event.preventDefault();
+})
+
+
