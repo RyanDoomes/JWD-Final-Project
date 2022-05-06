2aa3db9764e0b59f2e9a7296d5908294cba66ee8
Charlene Cornelius <chichiwepe@aol.com>
2022-05-06 16:49:05 -0400

    Event Handler

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 let task1 = new TaskManager;
-task1.addTask("take out the garbage", "Ryan", 05-05-2022, "take out at 6pm");
 
-console.log(task1);
+const newTaskForm = document.querySelector('.task-form');
+
