d447152bf52eb6ca7b1a9dc9c02892dab362d3aa
Charlene Cornelius <chichiwepe@aol.com>
2022-05-09 14:17:27 -0400

    new form

--- js/taskManager.js
+++ js/taskManager.js
@@ -1,15 +1,25 @@
 const createTaskHtml = (taskName, assignedTo, dueDate, taskNotes)=>{
-const newForm = `<li class="list-group-item">
-<div class="d-flex w-100 mt-2 justify-content-between align-items-center">
-    <h5>${taskName}</h5>
-    <span class="badge badge-danger">TODO</span>
+const newForm = `<form class="taskList">
+
+<div class="card" style="width: 22rem;">
+  <div class="card-body">
+    <h5 class="card-title">${taskName}</h5>
+    <br>
+    <h6 class="card-subtitle mb-2 text-muted" id="assignedToInput">${assignedTo}</h6>
+    <br>
+    <h6 class="card-subtitle mb-2 text-muted">${dueDate}</h6>
+    <p class="card-subtitle mb-2 text-muted">${taskNotes}</p>
+     <div class="row">
+      <div class="col">
+        <a href="#" class="btn btn-primary">Delete</a>
+      </div>
+      <div class="col">
+        <a href="#" class="btn btn-primary">Mark as done</a>
+      </div>
+    </div>
+  </div>
 </div>
-<div class="d-flex w-100 mb-3 justify-content-between">
-    <small>Assigned To:${assignedTo}</small>
-    <small>Due:${dueDate}</small>
-</div>
-<p>${taskNotes}</p>
-</li>`
+</form>`
 
 return newForm
 
@@ -34,6 +44,28 @@ class TaskManager {
   this.tasks.push(task);
   }
   
+  render(){
+   
+    const tasksHtmlList = [];
+
+    for(let i = 0; i < this.tasks.length; i++){
+      const task = this.tasks[1];
+
+      const date = new Date(task.dueDate);
+      const formattedDate = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
+
+      const taskHtml = createTaskHtml(task.id, task.taskName, task.assignedTo, formattedDate, task.taskNotes);
+
+      tasksHtmlList.push(taskHtml);
+    }
+
+    const tasksHtml = tasksHtmlList.join('\n');
+
+    const tasksList = document.querySelector('#tasksList');
+    tasksList.innerHTML = tasksHtml;
+
+
+  }
   
 
 }
